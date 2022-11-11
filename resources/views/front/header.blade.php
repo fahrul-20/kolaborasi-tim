5c9f36e33d1a045ea93470a2b5421ccbd49411f8
fahrulsw <fahrulshalahudin@gmail.com>
2022-11-11 18:05:22 +0700

    file upload

---
+++ resources/views/front/header.blade.php
@@ -0,0 +1,28 @@
+@extends('front.index')
+@section('content')
+<header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Yummy<span>.</span></h1>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="{{'home'}}">Home</a></li>
+          <li><a href="{{'menu'}}">Menu</a></li>
+          <li><a href="{{'reservasi'}}">Reservation</a></li>
+          <li><a href="{{'contact'}}">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+      <a class="btn-book-a-table" href="#book-a-table">Book a Table</a>
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+
+    </div>
+  </header>
+    
+@endsection
